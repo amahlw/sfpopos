7d88281315078c6847c3a70b81bcda16f41d4be5
Kash <amahlw@gmail.com>
2021-02-22 04:27:42 -0500

    dynamic content and footer

--- sfpopos/src/POPOSList.js
+++ sfpopos/src/POPOSList.js
@@ -1,40 +1,36 @@
 import React from 'react';
 import POPOSSpace from './POPOSSpace';
 import './POPOSList.css';
+import data from './sfpopos-data.json'
+
+// const titles = data.map((obj) => {
+//     return <h1>{obj.title}</h1>
+// })
+
 
 function POPOSList() {
-    return (
 
+    const spaces = data.map((obj) => {
+        // Deconstruct obj into properties
+        const { title, address, images } = obj
+
+        return (
+            <POPOSSpace
+                key={title} // The title could be a key
+                name={title}
+                address={address}
+                image={images[0]}
+            />
+        )
+    })
+    return (
         <div className="POPOSList">
-            {/* <h1>Keep your eye on this space for future content...</h1> */}
-            <POPOSSpace
-                name="50 California Street"
-                address="50 California St."
-                image="50-california-st.jpg" />
-            <POPOSSpace
-                name="100 Pine Street"
-                address="100 Pine St."
-                image="100-pine.jpg"
-            />
-            <POPOSSpace
-                name="101 California Street"
-                address="101 California ."
-                image="101-california.jpg" />
-            <POPOSSpace
-                name="343 Sansome Roof Garden"
-                address="343 Sansome St."
-                image="343-sansome-roof-garden.jpg" />
-            <POPOSSpace
-                name="525 Market Street Plaza"
-                address="525 Market St."
-                image="525-market-street-plaza.jpg" />
-            <POPOSSpace
-                name="Citigroup Center"
-                address="1 Sansome St."
-                image="citigroup-center.jpg"
-            />
+            { spaces}
         </div>
     )
 }
+
+
+
 
 export default POPOSList
